fc72ce3203de08b133714826b5ea462282806dd8
ByronAnalytics <kwbyron@byronanalytics.com>
2015-01-28 18:00:04 -0800

    Setup Plotting Scenes, Set separate constraints for iPhone Portrait and iPad Portrait. Created class for processing data outside view controllers

---
+++ Hive Maps/ProcessDataForPlotting.m
@@ -0,0 +1,17 @@
+//
+//  ProcessDataForPlotting.m
+//  Hive Maps
+//
+//  Created by Byron Analytics LLC on 1/28/15.
+//  Copyright (c) 2015 HiveMaps. All rights reserved.
+//
+
+#import "ProcessDataForPlotting.h"
+
+@implementation ProcessDataForPlotting
+
+
+
+
+
+@end
