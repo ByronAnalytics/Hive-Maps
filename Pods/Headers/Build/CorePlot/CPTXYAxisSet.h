8181604ce4337d1f764df2be0593215ff03b002b
ByronAnalytics <kwbyron@byronanalytics.com>
2015-02-11 12:42:05 -0800

    added pod files

--- Pods/Headers/Build/CorePlot/CPTXYAxisSet.h
+++ Pods/Headers/Build/CorePlot/CPTXYAxisSet.h
@@ -1,1 +1,11 @@
-../../../CorePlot/framework/Source/CPTXYAxisSet.h
+#import "CPTAxisSet.h"
+
+@class CPTXYAxis;
+
+@interface CPTXYAxisSet : CPTAxisSet {
+}
+
+@property (nonatomic, readonly, retain) CPTXYAxis *xAxis;
+@property (nonatomic, readonly, retain) CPTXYAxis *yAxis;
+
+@end
--- Pods/Headers/Build/CorePlot/NSNumberExtensions.h
+++ Pods/Headers/Build/CorePlot/NSNumberExtensions.h
@@ -1,1 +1,13 @@
-../../../CorePlot/framework/Source/NSNumberExtensions.h
+/** @category NSNumber(CPTExtensions)
+ *  @brief Core Plot extensions to NSNumber.
+ **/
+@interface NSNumber(CPTExtensions)
+
++(NSNumber *)numberWithCGFloat:(CGFloat)number;
+
+-(CGFloat)cgFloatValue;
+-(id)initWithCGFloat:(CGFloat)number;
+
+-(NSDecimalNumber *)decimalNumber;
+
+@end
